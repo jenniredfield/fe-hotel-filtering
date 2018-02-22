574f3e29c408e796978359ba31d5121e93fc8d60
Jen Bird <jenniredfield@hotmail.com>
2018-02-22 10:50:28 +0000

    Extracted unique facilities

--- src/Filter.js
+++ src/Filter.js
@@ -3,7 +3,24 @@ import React, {Component} from 'react';
 
 class Filter extends Component {
 
+
+    //need to add select dropdown or checkbox for facilities filter
+
     render () {
+
+        let hotels = this.props.hotels.slice() //copy of props
+        console.log(hotels)
+        let facilities = [];
+
+        hotels.map((hotel,i)=> {
+                hotel.Facilities.forEach(fac =>{
+                    if(!facilities.includes(fac)){
+                        facilities.push(fac)
+                    }
+                });
+        });
+        console.log(facilities);
+
         return (
             <div className="filterWrapper">
             
