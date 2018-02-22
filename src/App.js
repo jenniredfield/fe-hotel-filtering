8a2df26c3bd4d17f578178df408269316cd5aece
Jen Bird <jenniredfield@hotmail.com>
2018-02-22 10:37:33 +0000

    Added hotels files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import hotels from './hotels'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    hotels : hotels,
+  }
+
   render() {
+    console.log(hotels)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hotel Filtering</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/hotels.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        "Name": "Grand Hotel",
+        "StarRating": 5,
+        "Facilities": ["car park", "pool", "wifi", "gym", "restaurant", "bar"]
+    },
+    {
+        "Name": "Groove Hotel",
+        "StarRating": 3,
+        "Facilities": ["car park", "gym", "wifi", "restaurant"]
+    },
+    {
+        "Name": "Swag Hotel",
+        "StarRating": 1,
+        "Facilities": ["car park" ,"wifi"]
+    },
+    {
+        "Name": "Lambada Hotel",
+        "StarRating": 2,
+        "Facilities": ["car park" ,"wifi", "gym"]
+    },
+    {
+        "Name": "Heald Palace",
+        "StarRating": 2,
+        "Facilities": ["car park" ,"wifi", "gym"]
+    },
+    {
+        "Name": "Manchester Hotel",
+        "StarRating": 5,
+        "Facilities": ["car park", "pool", "wifi", "gym", "restaurant", "bar"]
+    },
+    {
+        "Name": "Hello World Hotel",
+        "StarRating": 4,
+        "Facilities": ["car park", "pool", "wifi", "gym", "restaurant" ]
+    },
+    {
+        "Name": "Infinite Loop Hotel",
+        "StarRating": 3,
+        "Facilities": ["car park", "pool", "wifi", "gym"]
+    },
+    {
+        "Name": "Aint that funky Hotel",
+        "StarRating": 3,
+        "Facilities": ["car park", "pool", "wifi", "gym"],
+    },
+    {
+        "Name": "Northcoders Hotel",
+        "StarRating": 1,
+        "Facilities": ["car park"]
+    }
+];
